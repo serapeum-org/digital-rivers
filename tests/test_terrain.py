eff2f48d186f60f3c8c517827db5b5897efe9e77
Mostafa Farrag <moah.farag@gmail.com>
2026-04-01 22:47:18 +0200

    fix issue in the test

--- tests/test_terrain.py
+++ tests/test_terrain.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import pytest
 from digitalrivers.terrain import Terrain
 
@@ -135,15 +136,23 @@ class TestAspect:
         assert vals.min() >= 0
 
 
-@pytest.mark.plot
-def test_create_color_relief(self):
-    arr = np.random.randint(0, 15, size=(10, 10))
-    dataset = Terrain.create_from_array(
-        arr, top_left_corner=(0, 0), cell_size=0.05, epsg=4326
-    )
-    color_relief = dataset.color_relief(band=0, color_table=self.df)
-    assert color_relief.band_count == 4
-    assert color_relief.band_color == {0: "red", 1: "green", 2: "blue", 3: "alpha"}
-    df = color_relief.stats()
-    assert all((0 < df["min"]) & (df["min"] <= 255))
-    assert all((0 < df["max"]) & (df["max"] <= 255))
+class TestColorRelief:
+
+    @pytest.mark.plot
+    def test_create_color_relief(self):
+        color_df = pd.DataFrame(
+            {
+                "values": [1, 3, 5, 7, 9],
+                "color": ["#709959", "#F2EEA2", "#F2CE85", "#C28C7C", "#D6C19C"],
+            }
+        )
+        arr = np.random.randint(0, 15, size=(10, 10))
+        dataset = Terrain.create_from_array(
+            arr, top_left_corner=(0, 0), cell_size=0.05, epsg=4326
+        )
+        color_relief = dataset.color_relief(band=0, color_table=color_df)
+        assert color_relief.band_count == 4
+        assert color_relief.band_color == {0: "red", 1: "green", 2: "blue", 3: "alpha"}
+        df = color_relief.stats()
+        assert all((0 < df["min"]) & (df["min"] <= 255))
+        assert all((0 < df["max"]) & (df["max"] <= 255))
